11c2bc07d2ffb29679e41e3426eab332d3db31ca
Peter Higgins <dante@dojotoolkit.org>
2009-03-27 00:54:58 +0000

    initial checkin of dojoType project code

---
+++ tests/_base.js
@@ -0,0 +1,4 @@
+dojo.provide("plugd.tests._base");
+if(dojo.isBrowser){
+	doh.registerUrl("dojotype.tests._base", dojo.moduleUrl("dojotype.tests", "_base.html"));
+}
---
+++ tests/module.js
@@ -0,0 +1,7 @@
+dojo.provide("dojotype.tests.module");
+
+try{
+	dojo.require("dojotype.tests._base");
+}catch(e){
+	doh.debug(e);
+}
