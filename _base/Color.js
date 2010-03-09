d89d0885f961cdf1a822c5af82599a2dc81d50e8
Peter Higgins <dante@dojotoolkit.org>
2010-03-09 13:55:23 +0000

    adding in Color conversion to strings and arrays

---
+++ _base/Color.js
@@ -0,0 +1,38 @@
+dojo.provide("dojotype._base.Color");
+(function(d){
+	/*=====
+	
+		dojotype.Color = function(){
+			// summary: 
+			//		A class which adds color-conversion capability to 
+			//		strings and arrays. An array of length 4 or 3 can be converted
+			//		to a string and vice versa, from their native prototypes.
+			//		
+			//		See `dojo.Color` for the available methods and appropriate documentation.
+			//		
+			// example:
+			//	[255, 255, 255].toCss(); // returns "#000000"
+			//
+			// example:
+			//	"#000000".toRgb(); // returns array: [255, 255, 255]
+			//
+		}
+	
+	=====*/
+	
+	// one color instance to use for conversions
+	var c = new dojo.Color(),
+		setup = function(meth){
+			if(!this[meth]){
+				this[meth] = function(){
+					c.setColor(this);
+					return c[meth].apply(c, arguments);
+				}
+			}
+		}
+	;
+	
+	dojo.forEach(["toRgb", "toRgba", "toHex"], setup, String.prototype);
+	dojo.forEach(["toCss"], setup, Array.prototype);
+	
+})(dojo);
